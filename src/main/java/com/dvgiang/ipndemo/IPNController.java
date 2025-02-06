aaa9c064ce287d2dd31258635da3ba47d7b290f5
Giang <140945898+dauvangiang@users.noreply.github.com>
2025-02-06 14:05:22 +0700

    test ipn api

--- src/main/java/com/dvgiang/ipndemo/IPNController.java
+++ src/main/java/com/dvgiang/ipndemo/IPNController.java
@@ -1,7 +1,9 @@
 package com.dvgiang.ipndemo;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +13,29 @@ public class IPNController {
     public String hello() {
         return "Hello World";
     }
+
+    @GetMapping
+    public ResponseEntity<Object> getIpn(
+            @RequestParam String pg_amount,
+            @RequestParam String pg_currency,
+            @RequestParam String pg_merchant_id,
+            @RequestParam String pg_order_info,
+            @RequestParam String pg_order_reference,
+            @RequestParam String pg_card_number,
+            @RequestParam String pg_card_holder_name,
+            @RequestParam String pg_payment_channel,
+            @RequestParam String pg_transaction_number,
+            @RequestParam String pg_issuer_txn_reference,
+            @RequestParam String pg_issuer_code,
+            @RequestParam String error_code,
+            @RequestParam String pg_issuer_response_code,
+            @RequestParam String pg_paytime,
+            @RequestParam String session_id,
+            @RequestParam String mac_type,
+            @RequestParam String mac
+    ) {
+        System.err.println(pg_transaction_number);
+        return ResponseEntity.ok("OK");
+    }
+
 }
