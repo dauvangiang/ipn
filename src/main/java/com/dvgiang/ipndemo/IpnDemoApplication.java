5295d8ebd9f433e6ba18886e434e6506af9269c6
Giang <140945898+dauvangiang@users.noreply.github.com>
2025-02-06 11:37:21 +0700

    first commit

---
+++ src/main/java/com/dvgiang/ipndemo/IpnDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dvgiang.ipndemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpnDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpnDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dvgiang/ipndemo/IpnDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvgiang.ipndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpnDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
