77fefab7cdd63e6ef490c749197fc315d08f3539
Giang <140945898+dauvangiang@users.noreply.github.com>
2025-02-06 23:51:31 +0700

    Update IPNController.java

--- src/main/java/com/dvgiang/ipndemo/IPNController.java
+++ src/main/java/com/dvgiang/ipndemo/IPNController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IPNController {
     @GetMapping("/hello")
     public String hello() {
+        System.err.println("Test logs!");
         return "Hello World";
     }
 
