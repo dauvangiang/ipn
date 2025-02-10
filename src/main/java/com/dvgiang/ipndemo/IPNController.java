1ce54a4fb3a890b0d814ad153a34fa563e49893b
Giang <140945898+dauvangiang@users.noreply.github.com>
2025-02-10 15:24:00 +0700

    test

--- src/main/java/com/dvgiang/ipndemo/IPNController.java
+++ src/main/java/com/dvgiang/ipndemo/IPNController.java
@@ -1,7 +1,21 @@
 package com.dvgiang.ipndemo;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/ipn")
@@ -35,6 +49,35 @@ public class IPNController {
     ) {
         System.err.println(pg_transaction_number);
         return ResponseEntity.ok("OK");
+    }
+
+    @GetMapping("/pay")
+    public String getPay() {
+        String text = "00020101021238570010A000000727012700069704220113VQRQAAWFI27760208QRIBFTTA530370454061000005802VN62190107NPS68690804Test63042F6B";
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        Map<EncodeHintType, Object> hints = new HashMap<>();
+        hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+
+        try {
+            // Tạo ma trận QR Code
+            BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, 500, 500, hints);
+
+            // Chuyển thành BufferedImage
+            BufferedImage qrImage = MatrixToImageWriter.toBufferedImage(bitMatrix);
+
+            // Chuyển ảnh QR thành Base64
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(qrImage, "png", baos);
+            byte[] imageBytes = baos.toByteArray();
+
+            String base64QR = Base64.getEncoder().encodeToString(imageBytes);
+            return "<html><body>" +
+                    "<h2>QR Code</h2>" +
+                    "<img src='data:image/png;base64," + base64QR + "' alt='QR Code' />" +
+                    "</body></html>";
+        } catch (WriterException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @PostMapping
